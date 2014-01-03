70cfa568509b19326140af7d5a9b7d9464299c7b
indivisible-irl <github@indivisibleindustries.com>
2014-01-03 10:03:02 +0000

    update to reflect config file moves

--- src/com/indivisible/twistedserveradmin/servers/Server.java
+++ src/com/indivisible/twistedserveradmin/servers/Server.java
@@ -5,6 +5,8 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import com.indivisible.twistedserveradmin.config.ServerInfo;
+import com.indivisible.twistedserveradmin.config.ServerProperties;
 import com.indivisible.twistedserveradmin.query.ServerQuery;
 
 /**
