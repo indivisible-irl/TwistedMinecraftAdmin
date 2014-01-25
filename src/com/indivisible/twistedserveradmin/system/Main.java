e9f5c7c1a2b67f927bf0d7b91ba8515c3ef00df5
indivisible-irl <github@indivisibleindustries.com>
2014-01-25 23:56:26 +0000

    exits on root file failure, started collecting status codes

--- src/com/indivisible/twistedserveradmin/system/Main.java
+++ src/com/indivisible/twistedserveradmin/system/Main.java
@@ -3,6 +3,7 @@ package com.indivisible.twistedserveradmin.system;
 import java.util.ArrayList;
 import java.util.List;
 import com.indivisible.twistedserveradmin.commands.CmdHandeler;
+import com.indivisible.twistedserveradmin.files.FileGetter;
 
 
 public class Main
@@ -10,6 +11,13 @@ public class Main
 
     public static void main(String[] argsArray)
     {
+        // ensure needed files exist
+        boolean rootFilesExist = FileGetter.ensureRootFilesExist();
+        if (!rootFilesExist)
+        {
+            System.exit(1);
+        }
+        // gather and pass args to commands
         CmdHandeler cmds = new CmdHandeler();
         List<String> argsList = makeArgsList(argsArray);
         boolean result = cmds.processCommand(argsList);
@@ -19,10 +27,8 @@ public class Main
         }
         else
         {
-            System.exit(1);
+            System.exit(2);
         }
-
-
     }
 
 
@@ -35,4 +41,18 @@ public class Main
         }
         return argsList;
     }
+
+
+    /*
+     * Status codes:
+     * 
+     * 0  - all ok
+     * 1  - failed ensuring root files exist
+     * 2  - failed to parse command properly
+     * 
+     * 10 - error reading/creating root files
+     * 11 - error reading server list file (ServerCollector constructor)
+     * 12 - error reading server list file (IOException)
+     * 13 - no folders to work with
+     */
 }
